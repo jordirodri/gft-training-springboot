4436a71c5495eafbd84c51187f9be2147f3a4a24
González Giménez <mion@gft.com>
2026-03-16 15:51:00 +0100

    Commit Mario

--- src/main/java/org/example/gfttrainingspringboot/GftTrainingSpringbootApplication.java
+++ src/main/java/org/example/gfttrainingspringboot/GftTrainingSpringbootApplication.java
@@ -8,6 +8,7 @@ public class GftTrainingSpringbootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GftTrainingSpringbootApplication.class, args);
+        System.out.println("Modificación Mario");
     }
 
 }
