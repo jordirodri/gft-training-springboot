832ad7059a58257f37473b709041f5deb3f24c9c
MUMP <miguelmonpi@gmail.com>
2026-03-16 15:56:25 +0100

    1ºCommit

--- src/main/java/org/example/gfttrainingspringboot/GftTrainingSpringbootApplication.java
+++ src/main/java/org/example/gfttrainingspringboot/GftTrainingSpringbootApplication.java
@@ -8,6 +8,8 @@ public class GftTrainingSpringbootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GftTrainingSpringbootApplication.class, args);
+
+        System.out.print("Miguel");
     }
 
 }
