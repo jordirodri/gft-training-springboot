369d57eddcff9ab343c0ce9161a03e90f8d0430d
JIRZ <jordi.rodriguez@gft.com>
2026-03-16 16:21:38 +0100

    test pipeline tests verify

---
+++ src/main/java/org/example/gfttrainingspringboot/JordiService.java
@@ -0,0 +1,9 @@
+package org.example.gfttrainingspringboot;
+
+public class JordiService {
+
+    public String getInfo(){
+        return "test getInfo";
+    }
+
+}
---
+++ src/test/java/org/example/gfttrainingspringboot/JordiServiceTest.java
@@ -0,0 +1,16 @@
+package org.example.gfttrainingspringboot;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JordiServiceTest {
+
+    @Test
+    void getInfo() {
+
+        JordiService result = new JordiService();
+
+        assertEquals("test getInfoAAAAA", result.getInfo());
+    }
+}
