4a1705f98c91c67c072ebe0e4c30b58242360eb8
mion <913mario@gmail.com>
2026-03-16 15:54:45 +0100

    commit m 2

--- src/main/java/org/example/gfttrainingspringboot/GftTrainingSpringbootApplication.java
+++ src/main/java/org/example/gfttrainingspringboot/GftTrainingSpringbootApplication.java
@@ -8,7 +8,7 @@ public class GftTrainingSpringbootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GftTrainingSpringbootApplication.class, args);
-        System.out.println("Modificación Mario");
+        System.out.println("Modificación Mario 2");
     }
 
 }
