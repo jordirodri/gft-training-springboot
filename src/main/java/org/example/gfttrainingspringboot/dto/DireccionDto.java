5f46cadd4c12c333f1c2204e13c433e89561de8e
aozq <vazquezcoboa@gmail.com>
2026-03-16 16:08:29 +0100

    Prueba Branch Alejandro Vazquez

--- src/main/java/org/example/gfttrainingspringboot/dto/DireccionDto.java
+++ src/main/java/org/example/gfttrainingspringboot/dto/DireccionDto.java
@@ -5,13 +5,15 @@ public class DireccionDto {
     private String numero;
     private String calle;
     private int codigoPostal;
+    private String localidad;
 
     public DireccionDto() {
     }
 
-    public DireccionDto(String numero, String calle, int codigoPostal) {
+    public DireccionDto(String numero, String calle, int codigoPostal, String localidad) {
         this.numero = numero;
         this.calle = calle;
         this.codigoPostal = codigoPostal;
+        this.localidad = localidad;
     }
 }
