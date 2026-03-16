5901ef649fb17c8cbd8f9f14f4f28cfc83eb7568
miranda9932 <albertmirandabayes@gmail.com>
2026-03-16 15:56:30 +0100

    prueba1

--- src/main/java/org/example/gfttrainingspringboot/GftTrainingSpringbootApplication.java
+++ src/main/java/org/example/gfttrainingspringboot/GftTrainingSpringbootApplication.java
@@ -2,12 +2,19 @@ package org.example.gfttrainingspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GftTrainingSpringbootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GftTrainingSpringbootApplication.class, args);
     }
 
+    @GetMapping("/")
+    public String home() {
+        return "hola soy albert";
+    }
 }
