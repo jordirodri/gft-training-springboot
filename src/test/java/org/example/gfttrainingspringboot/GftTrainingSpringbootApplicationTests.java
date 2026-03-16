8bead23c87079b2dfe616c300d410758888c5793
JIRZ <jordi.rodriguez@gft.com>
2026-03-16 15:38:35 +0100

    initial springboot

---
+++ src/test/java/org/example/gfttrainingspringboot/GftTrainingSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gfttrainingspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GftTrainingSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
