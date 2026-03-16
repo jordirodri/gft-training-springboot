c0e63e3ad2d9ff2055d958bf573d2953286a8b28
Gutiérrez Martínez <xrgz@gft.com>
2026-03-16 15:51:13 +0100

    Creación de la rama (XAVI)

--- src/main/java/org/example/gfttrainingspringboot/GftTrainingSpringbootApplication.java
+++ src/main/java/org/example/gfttrainingspringboot/GftTrainingSpringbootApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GftTrainingSpringbootApplication {
-
+    // Prueba
     public static void main(String[] args) {
         SpringApplication.run(GftTrainingSpringbootApplication.class, args);
     }
