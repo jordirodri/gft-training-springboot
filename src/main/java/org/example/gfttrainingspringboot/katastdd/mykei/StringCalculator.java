a10e8f926470c08ef185e53fde93c8477ff536da
Miguel Montero Piñeiro <132678438+Mykei4@users.noreply.github.com>
2026-03-17 14:34:52 +0100

    StringCalculator.java

--- src/main/java/org/example/gfttrainingspringboot/katastdd/mykei/StringCalculator.java
+++ src/main/java/org/example/gfttrainingspringboot/katastdd/mykei/StringCalculator.java
@@ -1,4 +1,33 @@
-package org.example.gfttrainingspringboot.katastdd.mykei;
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class StringCalculator {
+
+    public int add(String input) {
+        if (input == null || input.isEmpty()) return 0;
+
+        String delimiter = ",|\n";
+
+        if (input.startsWith("//")) {
+            int i = input.indexOf("\n");
+            delimiter = input.substring(2, i);
+            input = input.substring(i + 1);
+        }
+
+        int sum = 0;
+        List<Integer> negatives = new ArrayList<>();
+        for (String n : input.split(delimiter)) {
+            int num = Integer.parseInt(n);
+            if (num < 0) negatives.add(num);
+            else if (num < 1000) sum += num;
+        }
+
+        if (!negatives.isEmpty()) {
+            throw new IllegalArgumentException("Negativos: " + negatives);
+        }
+
+        return sum;
+    }
 }
